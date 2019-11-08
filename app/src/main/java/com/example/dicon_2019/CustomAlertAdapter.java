4caddfb2c103d8d59c1621400d6f8e79e30ef26d
kimdonguke <rlaehddnr100@gmail.com>
2019-11-08 18:30:21 +0900

    arduino bluetooth update

--- app/src/main/java/com/example/dicon_2019/CustomAlertAdapter.java
+++ app/src/main/java/com/example/dicon_2019/CustomAlertAdapter.java
@@ -1,24 +1,23 @@
-package com.example.dicon_2019;
-
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
-
-public class CustomAlertAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
-
-    @NonNull
-    @Override
-    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int i) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-}
+//package com.example.dicon_2019;
+//
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.ViewGroup;
+//
+//public class CustomAlertAdapter extends RecyclerView.Adapter<CustomAlertAdapter.ViewHolder>{
+//    @NonNull
+//    @Override
+//    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int i) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//}
--- app/src/main/java/com/example/dicon_2019/MainActivity.java
+++ app/src/main/java/com/example/dicon_2019/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 intent=new Intent(MainActivity.this,Make_fragActivity.class);
-
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }
         });
